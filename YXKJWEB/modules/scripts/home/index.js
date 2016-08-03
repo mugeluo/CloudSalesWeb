76fc3b4e1911fed361e7ebcd933953cb699d5402
song19940906 <2079972693@qq.com>
2016-08-03 13:23:28 +0800

    8.3云销web

--- YXKJWEB/modules/scripts/home/index.js
+++ YXKJWEB/modules/scripts/home/index.js
@@ -11,14 +11,17 @@
             ObjectJS.getWidthSize();
         });        
 
+        var array = [{ ask: "问：数据安全性和平台稳定性如何保障？<div class='ask-line'></div>", answer: "<div>第一，我们尽最大努力从技术角度保障客户数据的安全，定期对客户数据进行安全备份。</div><div>第二，我们购买的是阿里巴巴聚石塔服务器，阿里巴巴会为我们的服务器提供坚固的网络防护层。</div><div>第三，用户所有附件都上传至第三方服务器（七牛云存储），由第三方保障我们的附件安全。</div>" }, { ask: "问：二当家解决了企业哪些问题？<div class='ask-line'></div>", answer: "<div>答：第一，二当家提供CRM（客户管理）</div><div>第二，二当家还提供进销存管理</div><div>第三，二当家同时也提供了会员管理</div><div>一个应用解决了企业运营管理需求，实现企业多元化发展战略</div>" }, { ask: "问：使用二当家有哪些好处？<div class='ask-line'></div>", answer: "<div>答：第一，企业无需花费高额的研发费用定制软件，只需支出少量成本即可享受功能全面的二当家</div><div>第二，无需安装客户端，任何能网络良好的环境下都可登录二当家，实时查看和使用</div><div>第三，二当家还提供移动app，手机也能使用二当家</div>" }, { ask: "问：客户管理不便捷，用五当家怎么处理？<div class='ask-line'></div>", answer: "<div>答：可以使用五当家的CRM模块来管理客户信息，自定义客户阶段，随时随地处理跟进</div>" }];
+
         $(".sove-img div").click(function () {
-            var _this = $(this), ask = _this.data("ask"), answer = _this.data("answer");
+            var _this = $(this), id = _this.data("id");
             if (!_this.hasClass("hover")) {
                 $(".sove-img div").removeClass("hover");
                 _this.addClass("hover");
             };
-            $(".ask-answer .ask").html(ask);
-            $(".ask-answer .answer").html(answer);
+            var txt = array[id];
+            $(".ask").html(txt.ask);
+            $(".answer").html(txt.answer);
         });
 
         $(".dg-wrapper .outbound").click(function () {
